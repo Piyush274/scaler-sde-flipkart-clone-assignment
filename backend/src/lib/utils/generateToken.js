f76a703fbf6d366a73e13209680c2ea670598965
Piyush274 <pksangam888@gmail.com>
2026-04-15 21:34:37 +0530

    Refactor cookie settings in generateTokenAndSetCookie function for improved security and clarity

--- backend/src/lib/utils/generateToken.js
+++ backend/src/lib/utils/generateToken.js
@@ -9,13 +9,13 @@ export const generateTokenAndSetCookie=(userId,res)=>{
     const token=jwt.sign({userId},getJwtSecret(),{expiresIn:"15d"})
     //Created a token  with payload UserId and ecoded with JWT secret  
     
-    //Send as cookie to frontend browser and store it
-    res.cookie("jwt",token,{
-        maxAge:15*24*60*60*1000, //In millseconds
-        httpOnly:true, //Prevents XSS attack cross-site scripting attacks
-        sameSite:"strict", //CSRF attacks cross-site request forgery attacks
-        secure:process.env.NODE_ENV!=="development", //If not in development mode then secure
-    })
+    // Send as cookie to frontend browser and store it
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000, // In milliseconds
+        httpOnly: true, // Prevents XSS attacks
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    });
 
     return token;
 }
